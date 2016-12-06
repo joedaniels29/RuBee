640cc089ce2d83bffee87f8721d51d348172ace0
Joseph Daniels <joedaniels29@me.com>
2016-12-06 08:59:46 -0500

    That was easy

--- environment.sh
+++ environment.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 if [[ $(uname) -eq 'darwin' ]]; then
     brew install ruby --devel
+    exit 0
 elif [[ $(uname) -eq 'linux' ]]; then
     sudo apt-add-repository ppa:brightbox/ruby-ng
     sudo apt-get update
     sudo apt-get install ruby2.3 ruby2.3-dev
+    exit 0
 fi
